26f7fe6dd58a3d009d291ebb850ae206dacc5646
Dilip Bilawliya <Dilip92Bilawliya@gmail.com>
2023-11-07 18:43:02 +0530

    Modified page analyzer

--- lib/page_analyzer.rb
+++ lib/page_analyzer.rb
@@ -3,18 +3,25 @@ require 'nokogiri'
 
 class PageAnalyzer
 
-  attr_reader :url, :content
-
   def initialize(url)
     @url = url
   end
 
   def load
-    @doc = Nokogiri::HTML(URI.open(url))
+    @doc = Nokogiri::HTML(URI.open(@url))
   end
-  
-  def fetch_page
-    @content = download_page
+
+  def access_info
+    links = link_count
+    images = image_count
+    last_accessed = formatted_time(last_accessed_time)
+    [links, images, last_accessed]
+  end
+
+  private
+
+  def formatted_time(time)
+    time.strftime('%a %b %d %Y %H:%M UTC')
   end
 
   def link_count
@@ -29,23 +36,5 @@ class PageAnalyzer
 
   def last_accessed_time
     accessed_time ||= Time.now.utc
-  end
-
-  def access_info
-    {
-      links: link_count,
-      images: image_count,
-      last_accessed: formatted_time(last_accessed_time)
-    }
-  end
-
-  private
-
-  def download_page
-    URI.open(url).read
-  end
-
-  def formatted_time(time)
-    time.strftime('%a %b %d %Y %H:%M UTC')
   end
 end
